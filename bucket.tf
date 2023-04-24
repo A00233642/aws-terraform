9724f7b51306f8c0dad41d6351821525aa4b445b
A00233642 <Taiwoakintunde26@gmail.com>
2023-04-24 00:10:00 -0400

    first commit

--- bucket.tf
+++ bucket.tf
@@ -14,47 +14,47 @@ resource "aws_s3_bucket_versioning" "version1" {
 }
 
 #data "aws_s3_bucket" "data" {
- # bucket = "taiwo-testing123"
+# bucket = "taiwo-testing123"
 #}
 
-#data "aws_route53_zone" "test_zone" {
-#name         = "taiwo.com"
-# private_zone = false
-#}
+data "aws_route53_zone" "test_zone" {
+  name         = "taiwo.com"
+  private_zone = false
+}
 
-#resource "aws_route53_record" "akin" {
-#zone_id = data.aws_route53_zone.test_zone.id
-# name    = "bucket"
-# type    = "A"
-
-
-#alias {
-#   name                   = aws_elb.main.dns_name
-#   zone_id                = aws_elb.main.zone_id
-#   evaluate_target_health = true
-# }
+resource "aws_route53_record" "akin" {
+  zone_id = data.aws_route53_zone.test_zone.id
+  name    = "bucket"
+  type    = "A"
 
 
-#}
+  alias {
+    name                   = aws_elb.main.dns_name
+    zone_id                = aws_elb.main.zone_id
+    evaluate_target_health = true
+  }
+
+
+}
 
 #resource "aws_s3_object" "first_item" {
- # bucket = data.aws_s3_bucket.data.id
-  #key    = "terraform.tf"
-  #source = "terraform.tf"
+# bucket = data.aws_s3_bucket.data.id
+#key    = "terraform.tf"
+#source = "terraform.tf"
 
 #}
 
-#resource "aws_elb" "main" {
-#name               = "foobar-terraform-elb"
-#availability_zones = ["us-east-1c"]
+resource "aws_elb" "main" {
+name               = "foobar-terraform-elb"
+availability_zones = ["us-east-1c"]
 
-#listener {
-#instance_port     = 80
-#instance_protocol = "http"
-#lb_port           = 80
-#lb_protocol       = "http"
-#}
+listener {
+instance_port     = 80
+instance_protocol = "http"
+lb_port           = 80
+lb_protocol       = "http"
+}
 
-#}
+}
 
 
--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 #variable "aws_region" {
- # type    = string
- # default = "us-east-1"
+# type    = string
+# default = "us-east-1"
 #}
 
 #variable "instance_type" {
@@ -8,23 +8,23 @@
 #default = t2.medium 
 #}
 
-data "aws_ami" "amazon-2"{
-    most_recent = true
-    filter {
-      name = "description"
-      values = ["Amazon Linux 2 Kernel * x86_64 HVM gp2"]
-    }
+data "aws_ami" "amazon-2" {
+  most_recent = true
+  filter {
+    name   = "description"
+    values = ["Amazon Linux 2 Kernel * x86_64 HVM gp2"]
+  }
 
-    owners = [ "amazon" ]
+  owners = ["amazon"]
 
 }
 
 resource "aws_instance" "first-ec2" {
-    #get latest ami
-    ami = data.aws_ami.amazon-2.id
-    instance_type = "t2.micro"
-    tags = {
-      "name" = "pragra-totorials"
-    }
-  
+  #get latest ami
+  ami           = data.aws_ami.amazon-2.id
+  instance_type = "t2.micro"
+  tags = {
+    "name" = "pragra-totorials"
+  }
+
 }
